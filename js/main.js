655ef5046d535974a0a95283670addee2d063c8f
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2022-09-19 09:26:01 -0300

    criação e estilização do conteúdo principal

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$('.home-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    autoplay:true
+ })
+ 
+ $('.product-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    items:1,
+    autoplay:true,
+    center:false,
+ })
+
+
+//  responsive:{
+//     0:{
+//      items:1,
+//      nav:true
+//     },
+//     550:{
+//      items:2
+//     },
+//     1000:{
+//      items:3
+//     }
+//   }
